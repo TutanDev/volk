666a735a2d7f03de01ab1c52b23663d4db6983c5
tutan09 <andrespino.95@gmail.com>
2024-04-27 17:21:39 +0200

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+
+-- Volk premake5 script
+
+project "Volk"
+	kind "StaticLib"
+	language "C"
+	staticruntime "on"
+	warnings "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"volk.h",
+		"volk.c",
+	}
+
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
